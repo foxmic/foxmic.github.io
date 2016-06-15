96a9f7708d48e1adf1b02ccdf020058944b5ec2c
foxmic <michelfuchs0@gmail.com>
2016-06-15 14:20:31 +1000

    second js test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 function fetchJSON() {
-var url = 'https://api.spotify.com/v1/tracks/1zHlj4dQ8ZAtrayhuDDmkY';
+var url = 'https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj';
 var success_callback = function (songFromAPIRequest){
   // The response has already been parsed for us.
   alert("Song Name: " + songFromAPIRequest.name);
