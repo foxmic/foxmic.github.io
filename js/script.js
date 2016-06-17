c5e93db97d276bc1948b417addbf6adbbb3f86f5
foxmic <michelfuchs0@gmail.com>
2016-06-17 16:51:41 +1000

    jquery load v2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-$('.buttondiv a').on('click', function(e){
+$('#buttondiv a').on('click', function(e){
   e.preventDefault();
   var url = this.href;
 
-  $('.content').remove();
-  $('.content').load(url + '.content').hide().fadeIn('slow');
+  $('.speccontent').remove();
+  $('.content').load(url + '.speccontent').hide().fadeIn('slow');
 })
