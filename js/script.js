6b89daca2c671b59afa74b23c4abccb81d165ce4
foxmic <michelfuchs0@gmail.com>
2016-06-17 16:36:45 +1000

    jquery load v1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 
-function fetchJSON() {
-var url = 'https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj';
-var success_callback = function (songFromAPIRequest){
-  // The response has already been parsed for us.
-  alert("Song Name: " + songFromAPIRequest.name);
-};
- 
-$.getJSON(url, success_callback);
-}
+$('.buttondiv a').on('click', function(e){
+  e.preventDefault();
+  var url = this.href;
+
+  $('.content').remove();
+  $('.content').load(url + '.content').hide().fadeIn('slow');
+})
