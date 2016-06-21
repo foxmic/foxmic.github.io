8e6f9463a3448a9b41830e4fbf1dd0ba6bbece4e
foxmic <michelfuchs0@gmail.com>
2016-06-21 10:26:22 +1000

    load jquery latest

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ $('#buttondiv a').on('click', function(e) {
   var url = this.href;
   e.preventDefault();
 
+  $('a.active').removeClass('active');
+  $(this).addClass('active')
+
   
   $('#content').remove();
   $('#container').load(url + ' #content').hide().fadeIn('slow');
